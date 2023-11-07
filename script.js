63fa4b3242385822496d5e1955032b81bffd9944
TejashriChouke <139746868+TejashriChouke@users.noreply.github.com>
2023-11-07 20:23:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+// Get the user agent string
+        var userAgent = navigator.userAgent;
+
+        // Detect the browser name and version
+        var browserName;
+        var version;
+
+        if (userAgent.indexOf("Chrome") != -1) {
+            // Chrome
+            browserName = "Chrome";
+            version = userAgent.match(/Chrome\/(\d+\.\d+)/)[1];
+        } else if (userAgent.indexOf("Firefox") != -1) {
+            // Firefox
+            browserName = "Firefox";
+            version = userAgent.match(/Firefox\/(\d+\.\d+)/)[1];
+        } else if (userAgent.indexOf("Edge") != -1) {
+            // Microsoft Edge
+            browserName = "Microsoft Edge";
+            version = userAgent.match(/Edge\/(\d+\.\d+)/)[1];
+        } else if (userAgent.indexOf("Safari") != -1 && userAgent.indexOf("Version") != -1) {
+            // Safari
+            browserName = "Safari";
+            version = userAgent.match(/Version\/(\d+\.\d+)/)[1];
+        } else {
+            // Default to displaying the user agent string
+            browserName = "Unknown";
+            version = "N/A";
+        }
+
+        // Update the content of the browser-info div
+        var browserInfoDiv = document.getElementById("browser-info");
+        browserInfoDiv.textContent = "You are using " + browserName + " version " + version;
